d1d08120a2ab50c9426c4e2041dbce44108b4377
Hyunwoo Kim <eric.hyunwoo@gmail.com>
2022-01-08 00:54:10 +0900

    Add files via upload

---
+++ Kotlin/백준/Baekjoon.kt
@@ -0,0 +1,8 @@
+//1-7
+fun main(args: Array<String>) {
+    val input:String? = readLine()
+    val split:List<String> = input!!.split(" ".toRegex())
+    val a = split[0].toInt()
+    val b = split[1].toInt()
+    print(a*b)
+}
