c56a86816ff229818a16131c9f62113079e03a5a
Hyunwoo Kim <eric.hyunwoo@gmail.com>
2022-01-13 02:52:18 +0900

    Update HW2.dart

--- Flutter/HW2.dart
+++ Flutter/HW2.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xffFFFFFF)),
       home: Scaffold(
         appBar: AppBar(
           iconTheme: IconThemeData(
@@ -17,18 +18,15 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.white,
           centerTitle: false,
-          title: Container(
-            child: Text('금호동3가', style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-              letterSpacing: 1,
-            ),),
-          ),
-          actions: <Widget> [
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 0, 170, 0),
-              child: IconButton(
+          title: Row(
+            children: const [
+              Text('금호동3가', style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1,
+              ),),
+              IconButton(
                 icon: Icon(
                   Icons.keyboard_arrow_down_rounded,
                   color: Colors.black,
@@ -36,7 +34,9 @@ class MyApp extends StatelessWidget {
                 ),
                 onPressed: null,
               ),
-            ),
+            ],
+          ),
+          actions: <Widget> [
             Container(
               padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
               child: IconButton(
@@ -71,6 +71,73 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ]
+        ),
+        body: SizedBox(
+          height: 160,
+          child: Row(
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                width: 150,
+                height: 140,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20), // Image border
+                  child: SizedBox.fromSize(
+                    size: Size.fromRadius(48), // Image radius
+                    child: Image.asset('camera.png', fit: BoxFit.cover),
+                  ),
+                )
+              ),
+              SizedBox(
+                //color: Colors.black,
+                width: 350,
+                height: 140,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 0, 63, 0),
+                      child: Text(
+                          '캐논 DSLR 100D (단렌즈,',
+                          style: TextStyle(color: Colors.black, fontSize: 20, letterSpacing: 1.3, wordSpacing: 1.5, fontWeight: FontWeight.w500)
+                      ),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      padding: EdgeInsets.fromLTRB(0, 0, 90, 0),
+                      child: Text(
+                          '충전기 16기가SD 포함)',
+                          style: TextStyle(color: Colors.black, fontSize: 20, letterSpacing: 1.3, wordSpacing: 1.5, fontWeight: FontWeight.w500)
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 3, 110, 0),
+                      child: Text(
+                          '성동구 행당동 . 끌올 10분 전',
+                          style: TextStyle(color: Color(0xFF8C8C8C), fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.5)
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 3, 217, 0),
+                      child: Text(
+                          '210,000원',
+                          style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w800, letterSpacing: 1.3)
+                      ),
+                    ),
+                    Row (
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Image.asset('heart.png'),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(2, 0, 40, 0),
+                          child: Text('4', style: TextStyle(color: Color(0xff5e5e5e))),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       )
     );
